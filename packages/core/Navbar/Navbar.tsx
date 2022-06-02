7013d7afd55b9fc7698b83a7fd0dbb963a8c4824
Eduard-Constantin Stoica <56805725+edyykp@users.noreply.github.com>
2022-06-03 00:36:55 +0100

    chore: implement various things (#4)
    Co-authored-by: b09217es <eduard.stoica@student.manchester.ac.uk>

--- packages/core/Navbar/Navbar.tsx
+++ packages/core/Navbar/Navbar.tsx
@@ -1,9 +1,10 @@
+import { useEffect, useState } from 'react';
 import Link from 'next/link';
 import { FaBars } from 'react-icons/fa';
 import { Link as LinkS } from 'react-scroll';
 
-import { useMenu, useSiteProperties } from '@packages/config';
 import { Button } from '@packages/components';
+import { useMenu, useSiteProperties } from '@packages/config';
 
 import styles from './Navbar.module.css';
 
@@ -15,15 +16,34 @@ export const Navbar = ({ toggle }: NavbarProps) => {
   const menuItems = useMenu();
   const valueOf = useSiteProperties();
 
+  const [scrollNav, setScrollNav] = useState(false);
+
+  const changeNav = () => {
+    if (window.scrollY >= 80) {
+      setScrollNav(true);
+    } else {
+      setScrollNav(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', changeNav);
+  }, []);
+
   const text = {
     logo: valueOf('site_title'),
   };
 
   return (
-    <nav data-testid="navigation-bar">
+    <nav
+      data-testid="navigation-bar"
+      className={`${scrollNav ? styles.backgroundColor : ''}`}
+    >
       <div className={styles.container} data-testid="navigation-bar-logo">
         <Link href="/">
-          <a className={styles.logo}>{text.logo}</a>
+          <a className={`${styles.logo} ${scrollNav ? styles.logoColor : ''}`}>
+            {text.logo}
+          </a>
         </Link>
         <div className={styles.mobileMenuIcon} data-testid="mobile-menu-icon">
           <FaBars className={styles.faBars} onClick={toggle} />
@@ -31,14 +51,19 @@ export const Navbar = ({ toggle }: NavbarProps) => {
         <ul className={styles.menu} data-testid="navigation-bar-menu">
           {menuItems.map((item, key) => (
             <li className={styles.menuItem} key={key}>
-              <LinkS to={item.url} className={styles.menuUrls}>
+              <LinkS
+                to={item.url}
+                className={`${styles.menuUrls} ${
+                  scrollNav ? styles.urlColor : ''
+                }`}
+              >
                 {item.title}
               </LinkS>
             </li>
           ))}
         </ul>
         <div className={styles.ctaContainer} data-testid="navigation-bar-cta">
-          <Button />
+          <Button theme={scrollNav ? 'secondary' : 'primary'} size="md" />
         </div>
       </div>
     </nav>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,18 @@
 import type { NextPage } from 'next';
+import { SignInForm } from '@packages/core';
 
 import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
-  return <div>hello</div>;
+  return (
+    <div className={styles.container}>
+      <video autoPlay muted loop className={styles.video} preload="auto">
+        <source src="/Video.mp4" />
+      </video>
+      <div className={styles.overlay} />
+      <SignInForm />
+    </div>
+  );
 };
 
 export default Home;
