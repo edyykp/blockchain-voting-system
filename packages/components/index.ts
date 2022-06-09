c8d32e49a58b898ab44d9e5c1e8d84d84e2281bb
b09217es <eduard.stoica@student.manchester.ac.uk>
2022-06-09 22:37:04 +0100

    feat: initial commit

--- packages/components/index.ts
+++ packages/components/index.ts
@@ -1,5 +1,6 @@
-export { Layout } from './Layout';
+export { Loading } from './Loading';
 export { Button } from './Button';
 export { Footer } from './Footer';
 export { Input } from './Input';
 export { Card } from './Card';
+export { Layout } from './Layout';
